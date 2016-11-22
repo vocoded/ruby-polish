0c41babad515d474faae3a56ea7bd7fe774173b2
Thomas Hutchings <vocoded@users.noreply.github.com>
2016-11-22 06:51:16 -0800

    More test cases

--- prune_string.rb
+++ prune_string.rb
@@ -2,6 +2,7 @@
 
 # Reference: http://codegolf.stackexchange.com/questions/100486/make-a-ceeeeeeee-program
 
+# Basic implementation, readable
 def prune_string(string, c)
   if string.to_s.empty?
     return ""
@@ -16,17 +17,23 @@ def prune_string(string, c)
   end
 end
 
-def prune_string_golf(string, c)
-  string.to_s.split("").reject.with_index { |x, i| i > 0 && x != c }.join("")
+# Expressive implementation, packing it in
+def prune_string_golf(s, c)
+  s.to_s.split("").reject.with_index { |x, i| i > 0 && x != c }.join("")
 end
 
 strings = [
   ["code.stackexchange.com", "e", "ceee"],
-  ["laughter is the best medicine", "i", "liii"]
+  ["laughter is the best medicine", "i", "liii"],
+  ["Hello world!", "!", "H!"],
+  ["Hello world!", "z", "H"],
+  ["Test Cases", "s", "Tsss"],
 ]
 
 strings.each do |test|
-  result = prune_string_golf test[0], test[1]
+  result = prune_string test[0], test[1]
+  result_golf = prune_string_golf test[0], test[1]
   puts result + (result == test[2] ? ": PASSED" : ": FAILED")
+  puts result_golf + (result_golf == test[2] ? ": GOLF PASSED" : ": GOLF FAILED")
 end
 
